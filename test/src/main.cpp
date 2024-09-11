e88e4efff31157d1dfe46b93be832cc31ae21ca1
Guanghui Hu <120L052208@stu.hit.edu.cn>
2024-09-11 23:27:57 +0800

    dev: add utils test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,13 +1,20 @@
 #include "bitvec/core.h"
+#include "bitvec/utils.h"
 #include <gtest/gtest.h>
 
 TEST(BitVecTest, create) {
   auto foo = bv::BitVec(12, 32);
-  EXPECT_EQ(foo.to_string(), "c") << "fail" ;
-  auto bar = bv::BitVec(12, 32);
+  EXPECT_EQ(foo.to_string(), "000c");
+  auto bar = bv::BitVec(12);
+  EXPECT_EQ(bar.to_string(), "c");
 }
 
 TEST(BitVecTest, changable) {}
+
+TEST(BitVecUtils, min_len) {
+  EXPECT_EQ(bv::min_len(1), 1);
+  EXPECT_EQ(bv::min_len(1), 0);
+}
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
