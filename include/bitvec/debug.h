6680c79171774187e19531454974648a5ef9526c
Guanghui Hu <hugh2024@shanghaitech.edu.cn>
2024-09-15 11:27:45 +0800

    dev: add info, warn, error print func

--- include/bitvec/debug.h
+++ include/bitvec/debug.h
@@ -1,6 +1,11 @@
 #ifndef __BITVEC_DEBUG_H__
 #define __BITVEC_DEBUG_H__
 
+#define BV_LOG_LEVEL 0
+#define BV_LOG_INFO 1
+#define BV_LOG_WARN 2
+#define BV_LOG_ERROR 3
+
 namespace bv {
 
 // impliment in cc file, print and exit(-1)
@@ -15,5 +20,16 @@ namespace bv {
       error_impl(fmt::format(fmt_str, ##__VA_ARGS__).c_str());                 \
   } while (0)
 #endif
+
+#define BV_LOG(cond, fmt_str, ...)                                             \
+  do {                                                                         \
+    if (BV_LOG_LEVEL >= cond)                                                  \
+      fmt::println(fmt_str, ##__VA_ARGS__);                                    \
+  } while (0)
+
+#define BV_INFO(fmt_str, ...) BV_LOG(BV_LOG_INFO, fmt_str, __VA_ARGS__)
+#define BV_WARN(fmt_str, ...) BV_LOG(BV_LOG_WARN, fmt_str, __VA_ARGS__)
+#define BV_ERROR(fmt_str, ...) BV_LOG(BV_LOG_ERROR, fmt_str, __VA_ARGS__)
+
 };     // namespace bv
 #endif // __BITVEC_DEBUG_H__
